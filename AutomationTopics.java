77498b1213868a1697e0ea8427c630fc494300ba
bhavipa <bhavu4444@gmail.com>
2022-01-06 15:36:57 +0000

    my commit

---
+++ AutomationTopics.java
@@ -0,0 +1,16 @@
+package com.firstautomation;
+
+public class AutomationTopics {
+
+    public static void main(String[]args)
+    {
+        System.out.println("Programing Lauguage");
+        System.out.println("Whare Is Java Used");
+
+
+    }
+
+
+
+
+}
