b7b1d8473ab89d49ee79f3765786e58cf5d75bb3
bhavita <bhavu4444@gmail.com>
2022-01-08 13:50:04 +0000

    change in firstautomation

--- AutomationTopics.java
+++ AutomationTopics.java
@@ -4,12 +4,14 @@ public class AutomationTopics {
 
     public static void main(String[]args)
     {
-        System.out.println("Programing Lauguage");
         System.out.println("Whare Is Java Used");
         System.out.println("Java Features");
         System.out.println("Naming Conventions");
         System.out.println("Java Features");
         System.out.println("Naming Conventions");
+        System.out.println("Github Is Collabration Platform");
+
+
 
     }
 
